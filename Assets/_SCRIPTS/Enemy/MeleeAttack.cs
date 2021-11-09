d561650582a5618a83ef6fdc77a248c985a507cb
Jasmine Duerk <jasmine.duerk@gmail.com>
2021-11-09 14:51:27 -0500

    smoother attacks, can attack repeatedly if player doesn't get out of the way

--- Assets/_SCRIPTS/Enemy/MeleeAttack.cs
+++ Assets/_SCRIPTS/Enemy/MeleeAttack.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MeleeAttack : MonoBehaviour
 {
+    [HideInInspector] public bool currentlyMeleeAttacking = false;
+    private bool nearPlayer = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +20,43 @@ public class MeleeAttack : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
+    public void DoAttack()
+    {
+        Debug.Log("DOING ATTACK");
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.AppendInterval(transform.parent.GetComponent<Enemy>().meleeAttackInterval).OnComplete(() => AttackHelper());
+    }
+
+    private void AttackHelper()
+    {
+        if (!currentlyMeleeAttacking && nearPlayer)
+        {
+            transform.parent.GetComponent<Enemy>().MeleeAttack();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            nearPlayer = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !currentlyMeleeAttacking)
+        {
+            nearPlayer = true;
             transform.parent.GetComponent<Enemy>().MeleeAttack();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            nearPlayer = false;
         }
     }
 
