a92e3fbf4707d4c2f86c9814b48adeb001c79f4d
Jasmine Duerk <jasmine.duerk@gmail.com>
2021-11-19 15:09:52 -0500

    game over

--- Assets/_SCRIPTS/DamageCrystal.cs
+++ Assets/_SCRIPTS/DamageCrystal.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
 
 public class DamageCrystal : MonoBehaviour
 {
@@ -8,7 +11,7 @@ public class DamageCrystal : MonoBehaviour
     public bool enemiesDefeated = false;
     public GameObject destroyedParticleEffect;
     public GameObject crystalMesh;
-
+    public Image gameOverPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,5 +47,18 @@ public class DamageCrystal : MonoBehaviour
         crystalData.isMined = 1;
         destroyedParticleEffect.SetActive(true);
         crystalMesh.SetActive(false);
+        AlphafestGameOver();
+    }
+
+    private void AlphafestGameOver()
+    {
+        Sequence mySequence = DOTween.Sequence();
+
+        gameOverPanel.gameObject.SetActive(true);
+
+        TextMeshProUGUI deathText = gameOverPanel.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        mySequence.AppendInterval(3f);
+        mySequence.Append(gameOverPanel.DOFade(1f, 0.5f));
+        mySequence.Append(deathText.DOFade(1f, 0.5f));
     }
 }
